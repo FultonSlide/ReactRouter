366055f7f556abeec607d018b24e97727b7aaddb
Will <Wi_llo@hotmail.com>
2020-01-17 15:04:49 +1100

    experimented with Higher Order Components and added axios to play with fetching rest API data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about');
+    // }, 2000);
+
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Poke'Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
